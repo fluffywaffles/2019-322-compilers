d846f2fe7afba356a1bd51e8d7b3576c103923fa
fluffywaffles <skorlir@gmail.com>
2019-01-27 10:39:50 -0600

    L1: factor debug tracer into `trace_parse` helper

--- L1/src/L1.cpp
+++ L1/src/L1.cpp
@@ -7,9 +7,15 @@
 namespace peg = tao::pegtl;
 using grammar = peg::must<L1::grammar::program>;
 
+namespace debug {
+  void trace_parse (peg::file_input<> &in) {
+    peg::parse<grammar, peg::nothing, peg::tracer>(in);
+  }
+}
+
 int main (int argc, char ** argv) {
   assert(argc > 1 && "Wrong number of arguments passed to compiler.");
   peg::file_input<> in(argv[1]);
-  peg::parse<grammar, peg::nothing, peg::tracer>(in);
+  debug::trace_parse(in);
   return 0;
 }
